2b113913c73d5909aee824a55bf002b252c873fd
o0u0o <1553122309@qq.com>
2023-06-12 19:11:48 +0800

    first commit

---
+++ src/main/java/io/github/talelin/latticy/dto/spu/SpuDTO.java
@@ -0,0 +1,8 @@
+package io.github.talelin.latticy.dto.spu;/**
+ * TODO 
+ * 
+ * @author mac
+ * @date 2022/4/22 5:09 PM
+ */
+public class SpuDTO {
+}
---
+++ src/main/java/io/github/talelin/latticy/model/SpuDetailDO.java
@@ -0,0 +1,8 @@
+package io.github.talelin.latticy.model;/**
+ * TODO 
+ * 
+ * @author mac
+ * @date 2022/4/20 10:31 AM
+ */
+public class SpuDetailDO {
+}
